4ae9af4bdeb2a02f3615ac6ba221af972e6b764a
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-21 18:43:04 -0400

    Converted list data into Question objects

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from question_model import Question
+from data import question_data
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+question_bank = []
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for question in question_data:
+    text = question["text"]
+    answer = question["answer"]
+    new_question_object = Question(question=text, answer=answer)
+    question_bank.append(new_question_object)
