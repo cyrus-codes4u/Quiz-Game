4e7b624bd0f163402d58507e6801dafd70d18d28
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-23 13:43:10 -0400

    Implement quiz sequence to ask all questions in question_bank

--- quiz_brain.py
+++ quiz_brain.py
@@ -6,5 +6,7 @@ class QuizBrain:
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (true/false)? ")
-        return user_answer
+        input(f"Q.{self.question_number}: {current_question.text} (true/false)? ")
+
+    def still_has_questions(self):
+        return len(self.questions_list) > self.question_number
