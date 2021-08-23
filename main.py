618cc0dfa2ce8f58b0b3ee5a68192f78667c24f5
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-23 13:37:35 -0400

    Added QuizBrain class to ask questions and receive answers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -8,3 +9,6 @@ for question in question_data:
     answer = question["answer"]
     new_question_object = Question(question=text, answer=answer)
     question_bank.append(new_question_object)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
